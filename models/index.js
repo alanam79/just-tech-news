fd50503191b3b75e17a3a69d35864744372af068
alanam79 <halo2382001@gmail.com>
2022-06-21 14:10:49 -0400

    working on post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+
 module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
 
 // uses the specified middleware function like if we can have /user, 
 // now this middleware is called for all API’s having /user of this router.
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
