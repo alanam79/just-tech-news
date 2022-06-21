34c8be2eac8bec9555b2e8fd470ed2383a2a6caa
alanam79 <halo2382001@gmail.com>
2022-06-21 09:16:39 -0400

    final save of branch

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -53,6 +53,29 @@ router.post("/", (req, res) => {
     });
 });
 
+  router.post('/login', (req, res) => {
+      // Query operation, this validaates the login, 
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
   // expects { username:, 'Larantino', email: 'larantino.gmail.com', password: 'password123'}
@@ -79,7 +102,6 @@ router.put("/:id", (req, res) => {
     });
 });
 
-// DELETE /api/users/1
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
   User.destroy({
